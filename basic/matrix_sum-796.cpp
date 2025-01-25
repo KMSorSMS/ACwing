3ccaa09f219032cdf63d6812d3ebf799b63351ea
liam <yzwliam@126.com>
2025-01-25 10:58:08 +0800

    :zip: fin prefix sum 795,796

---
+++ basic/matrix_sum-796.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#define N 1010
+
+int matrix[N][N];
+
+int main()
+{
+	int n, m, q;
+	std::cin >> n >> m >> q;
+	// receive matrix input
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= m; j++) { std::cin >> matrix[i][j]; }
+	}
+	// precompute prefix sum
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= m; j++)
+		{
+			matrix[i][j] += matrix[i][j - 1] + matrix[i - 1][j] - matrix[i - 1][j - 1];
+		}
+	}
+	// then answer the queries
+	int lux = 0, luy = 0, rdx = 0, rdy = 0;
+	for(int i = 1; i <= q; i++)
+	{
+		std::cin >> lux >> luy >> rdx >> rdy;
+		std::cout << (matrix[rdx][rdy] - matrix[rdx][luy - 1] - matrix[lux - 1][rdy] +
+					  matrix[lux - 1][luy - 1]) << std::endl;
+	}
+}
---
+++ basic/prefix_sum-795.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define N 100010
+
+int nums[N], s[N];
+
+int main()
+{
+	int n, m;
+	std::cin >> n >> m;
+	for(int i = 1; i <= n; i++) std::cin >> nums[i];
+	// std::cout << "input_done m is " << m << " n is " << n << std::endl;
+	s[0] = 0;
+	for(int i = 1; i <= n; i++) s[i] = s[i - 1] + nums[i];
+    int l,r;
+	for(int i = 0; i < m ; i++) {
+        std::cin >> l >> r;
+        std::cout << (s[r] - s[l-1]) << std::endl;
+    }
+    return 0;
+}
