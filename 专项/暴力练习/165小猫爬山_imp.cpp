807e7664e16653cccf76fd66c082a1a6fe2d53b1
liam Yuan <yzwliam@126.com>
2025-08-07 17:44:30 +0800

    :sparkles: dfs 进行剪枝,首先是发现这个装箱问题,物品数量少,但是容积大,所以适合暴力搜索,但是需要优化 优化的思路就是剪枝,对于发现已经用车/用的箱子数量超过了已知的最小值,那么就可以放弃这个分支的遍历 同时,为了能更好的触发这一点,我们只需要类似于贪心,把体积大/重量大的物品放到前面去遍历(靠近根).这样很容易就会出现箱子超额

---
+++ 专项/暴力练习/165小猫爬山_imp.cpp
@@ -0,0 +1,70 @@
+/*
+翰翰和达达饲养了 N 只小猫，这天，小猫们要去爬山。
+经历了千辛万苦，小猫们终于爬上了山顶，但是疲倦的它们再也不想徒步走下山了（呜咕>_<）。
+翰翰和达达只好花钱让它们坐索道下山。
+索道上的缆车最大承重量为 W，而 N 只小猫的重量分别是 C1、C2……CN。
+当然，每辆缆车上的小猫的重量之和不能超过 W。
+
+每租用一辆缆车，翰翰和达达就要付 1 美元，所以他们想知道，最少需要付多少美元才能把这 N 只小猫都运送下山？
+
+输入格式
+第 1 行：包含两个用空格隔开的整数，N 和 W。
+
+第 2..N+1 行：每行一个整数，其中第 i+1 行的整数表示第 i 只小猫的重量 C_i。
+
+输出格式
+输出一个整数，表示最少需要多少美元，也就是最少需要多少辆缆车。
+
+数据范围
+1≤N≤18,
+1≤Ci≤W≤10^8
+
+输入样例：
+5 1996
+1
+2
+1994
+12
+29
+输出样例：
+2
+*/
+
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+const int N = 20;
+int n, w;
+int c_s[N], w_s[N];
+int min_ = 0x3f3f3f3f;
+
+void dfs(int depth, int pack)
+{
+	if(pack >= min_) return;
+	if(depth > n) min_ = pack;
+	for(int i = 1; i <= pack; i++)
+	{
+		if(w_s[i] >= c_s[depth])
+		{
+			w_s[i] -= c_s[depth];
+			dfs(depth + 1, pack);
+			w_s[i] += c_s[depth];
+		}
+	}
+	w_s[pack + 1] -= c_s[depth];
+	dfs(depth + 1, pack + 1);
+	w_s[pack + 1] += c_s[depth];
+}
+
+int main()
+{
+	cin >> n >> w;
+	for(int i = 1; i <= n; i++) { w_s[i] = w; }
+	for(int i = 1; i <= n; i++) { cin >> c_s[i]; }
+	sort(c_s + 1, c_s + n, [](int a, int b) { return a > b; });
+	dfs(1, 1);
+	cout << min_ << "\n";
+	return 0;
+}
