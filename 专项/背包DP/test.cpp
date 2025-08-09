a2adc4042355590070adbeaeb62df32eec9854ee
liam Yuan <yzwliam@126.com>
2025-08-09 20:41:50 +0800

    :sparkles: fin round 2 mock

--- 专项/背包DP/test.cpp
+++ 专项/背包DP/test.cpp
@@ -2,73 +2,60 @@
 #include <cstdio>
 #include <iostream>
 #include <utility>
+#include <cstring>
+#include <string>
+#include <vector>
 
 using namespace std;
-typedef pair<int, int> pii;
+const int STR_LEN = 10000;
+vector<string> S;
+char tmp_str[STR_LEN];
+int n;
 
-const int N = 1e5 + 10, M = 1e5 + 10;
-int h[N], e[M], ne[M];
-int idx = 1, n;
-int tasks[N];
-int w[N];
-int idx_task = 1;
-
-void add_edge(int a, int b)
-{
-	e[idx] = b, ne[idx] = h[a];
-	h[a] = idx++;
-}
-
-int main()
-{
+int main(){
 	cin >> n;
-	for(int i = 2; i <= n; i++)
-	{
-		int fa;
-		cin >> fa;
-		add_edge(fa, i);
-	}
-	for(int i = 1; i <= n; i++) { cin >> tasks[i]; }
-	for(int i = 1; i <= n; i++) { cin >> w[i]; }
-	sort(w + 2, w + n + 1), sort(tasks + 1, tasks + n + 1);
-	int max_ = max(tasks[n] - w[1], 0);
-	// printf("max: %d,task:%d,w:%d\n", max_, tasks[n], w[1]);
-	if(max_ == 0)
-	{
-		int best1_w = 0, task1_ = 0;
-		for(int i = 1; i <= n - 1; i++)
-		{
-			if(tasks[i] - w[i + 1] > 0)
-			{
-				best1_w = best1_w == 0 ? w[i + 1] : best1_w;
-				task1_ = tasks[i];
+	string op,s,s_end;
+	for(int i=1;i<=n;i++){
+		cin >> op >> s;
+		if(op == "ADD"){
+			int len;
+			cin >> len;
+			for(int i=0;i<=s.size()-len;i++){
+				for(int j=0;j<len;j++){
+					tmp_str[j] = s[i+j];
+				}
+				tmp_str[len] = 0;
+				S.push_back(tmp_str);
 			}
 		}
-		// printf("best1_w:%d,task1_:%d\n", best1_w, task1_);
-		max_ = task1_ - best1_w;
+		else{
+			int res = 0;
+			cin >> s_end;
+			for(string str : S){
+				if(str >= s && str <= s_end){
+					res++;
+				}
+			}
+			cout << res << "\n";
+		}
 	}
-	// printf("max1:%d,max2:%d\n", max1_, max2_);
-	cout << max_ << "\n";
+	// // debug
+	// for(string str: S){
+	// 	cout << str << "\n";
+	// }
+	return 0;
 }
 /*
-6
-1 1 1 1 1
-1 2 3 4 5 6
-4 1 3 3 5 1
+4
+ADD aacab 2
+QUERY aa ab
+ADD abc 1
+QUERY a b
 
-5 
-1 1 1 1
-100 1 4 5 10
-100 2 3 6 8
-
-5 
-1 1 1 1
-100 1 4 5 8
-100 2 3 6 8
-
-5 
-1 1 1 1
-100 1 4 7 8
-100 2 3 6 8
+4
+ADD aacab 2
+QUERY aa ac
+ADD abc 1
+QUERY a b
 
 */
