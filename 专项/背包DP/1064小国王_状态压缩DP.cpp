320e8be194555515ae0170cbb62348335a2e38c0
liam Yuan <yzwliam@126.com>
2025-08-09 12:28:45 +0800

    :sparkles: 学习状态压缩 DP 的经典问题,这里有个小坑是 LL,记得有时候想想可能的最大方案数量会超多少,可以设计例子试试 f[i][j][s]表示第 i 行总共 j 个国王,i 行状态是 s 的情况下的方案总数,它只和 i-1 行有关=f[i-1][j-cnt(s)][s_b] s_b 代表了所有 能和 i 行状态 s 共存的所有状态,他们的总和就能算出 f[i][j][s]

--- 专项/背包DP/1064小国王_状态压缩DP.cpp
+++ 专项/背包DP/1064小国王_状态压缩DP.cpp
@@ -9,7 +9,7 @@
 
 数据范围
 1≤n≤10,
-0≤k≤n2
+0≤k≤n^2
 
 输入样例：
 3 2
@@ -17,6 +17,72 @@
 16
 */
 
-const int N=12;
-int graph[N][N];
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
+typedef long long ll; 
+const int N = 12,K = 144, S = 1024;
+ll f[N][K][S];
+int n,k;
+vector<int> head[S];
+vector<int> state;
+int count_ones[S];
 
+bool check_legal(int a){
+    for(int i=0;i<n-1;i++){
+        //就一个条件，不能相邻有 1
+        if(((a >> i)&1) && ((a >> (i+1))&1)){
+            return false;
+        }
+    }
+    return true;
+}
+
+void cal_count_ones(int a){
+    int tmp = a;
+    while(tmp!=0){
+        if(tmp & 1) count_ones[a]++;
+        tmp >>= 1;
+    }
+}
+
+void init_head(){
+    // 先排查哪些是合格的 state
+    for(int i=0;i<(1<<n);i++){
+        if(check_legal(i)){
+            state.push_back(i);
+            cal_count_ones(i);
+            // printf("legal:%x,ones:%d\n",i,count_ones[i]);
+        }
+    }
+    for(int a: state){
+        for(int b : state){
+            if(((a&b)==0) && check_legal(a|b)){
+                head[a].push_back(b);
+            }
+        }
+    }
+}
+
+int main(){
+    cin >> n >> k;
+    init_head();
+    f[0][0][0] = 1;
+    for(int i=1;i<=n;i++){
+        for(int a:state){
+            int ones = count_ones[a];
+            for(int j=k; j>= ones;j--){
+                for(int b:head[a]){
+                    f[i][j][a] +=  f[i-1][j-ones][b];
+                }
+            }
+        }
+    }
+    ll res = 0;
+    for(int a: state){
+        res += f[n][k][a];
+    }
+    cout << res << "\n";
+    return 0;
+}
