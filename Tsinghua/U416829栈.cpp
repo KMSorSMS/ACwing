a3961944fe088116e94493d40f0c87c1ede701df
liam Yuan <yzwliam@126.com>
2025-09-02 17:03:27 +0800

    :sparkles: 尝试ac 失败

--- Tsinghua/U416829栈.cpp
+++ Tsinghua/U416829栈.cpp
@@ -7,15 +7,24 @@
 2 3 2
 3 3 1
 2 1 4
+
+6
+6
+10
+10
+4
+12
+12
 */
 
 #include <iostream>
+#include <cstdio>
 using namespace std;
 const int M=2e5+10,N=2e5+10;
 struct OPS{
     int op;
     int x,w,c;
-    bool if_rev;
+    // bool if_rev;
     OPS* op_pre;
     OPS* op_next;
 };
@@ -44,8 +53,9 @@ int main(){
                 stack_now.op_begin = &ops[i];
                 stack_now.op_last = &ops[i];
             }else{
-                if(stack_now.op_last->if_rev) {stack_now.op_last->op_pre = &ops[i]; ops[i].op_next = stack_now.op_last;}
-                else {stack_now.op_last->op_next = &ops[i]; ops[i].op_pre = stack_now.op_last;}
+                if(stack_now.op_last->op_pre==nullptr) {stack_now.op_last->op_pre = &ops[i];}
+                else {stack_now.op_last->op_next = &ops[i];}
+                ops[i].op_pre = stack_now.op_last;
                 stack_now.op_last = &ops[i];
             }
             cout << stack_now.sum << "\n";
@@ -54,24 +64,34 @@ int main(){
             cin >> x >> c;
             ops[i] = {op,x,w,c};
             stack_s& stack_now = sta[x];
-            OPS* op = stack_now.op_last;
+            OPS* op_ = stack_now.op_last;
             int sum=0;
+            bool is_pre = true;
             while(c>0){
-                if(op->op==1){
-                    if(op->c > c){
-                        op->c -= c;
+                if(op_->op==1){
+                    // printf("c:%d,x:%d,op_c:%d,op_w:%d\n",c,x,op_->c,op_->w);
+                    if(op_->c > c){
+                        op_->c -= c;
+                        sum += c*op_->w;
                         c=0;
-                        sum += c*op->w;
+                        // printf("inner c:%d,x:%d,op_c:%d,op_w:%d,sum:%d\n",c,x,op_->c,op_->w,sum);
                     }else{
-                        c -= op->c;
-                        sum += op->c*op->w;
-                        if(op->if_rev){
-                            stack_now.op_last = op->op_next;
-                        }else{
-                            stack_now.op_last = op->op_pre;
+                        c -= op_->c;
+                        sum += op_->c*op_->w;
+                        if(op_->op_next==nullptr){
+                            is_pre = true;
+                        }else if(op_->op_pre==nullptr){
+                            is_pre = false;
                         }
-                        stack_now.op_last->if_rev = op->if_rev;
-                        op = stack_now.op_last;
+                        if(!is_pre)
+                        {
+                            if(op_->op_next == op_->op_next->op_next) stack_now.op_last = op_->op_pre,is_pre = true;
+                            else stack_now.op_last = op_->op_next;
+                        }else{
+                            if(op_->op_pre == op_->op_pre->op_pre) stack_now.op_last = op_->op_next,is_pre = false;
+                            else stack_now.op_last = op_->op_pre;
+                        }
+                        op_ = stack_now.op_last;
                     }
                 }else{
                     cout << 1/0;
@@ -84,15 +104,18 @@ int main(){
             int y;
             cin >> x >> y;
             if(sta[y].op_begin != nullptr){
-                sta[y].op_last->if_rev = !sta[y].op_last->if_rev;
-                if(sta[y].op_last->if_rev) sta[y].op_last->op_next = sta[x].op_last;
+                if(sta[y].op_last->op_next==nullptr) sta[y].op_last->op_next = sta[x].op_last;
                 else sta[y].op_last->op_pre = sta[x].op_last;
+                if(sta[x].op_last->op_next==nullptr)  sta[x].op_last->op_next = sta[y].op_last;
+                else sta[x].op_last->op_pre = sta[y].op_last;
                 sta[y].op_last = sta[x].op_begin;
                 sta[y].sum += sta[x].sum;
+                // printf("x:%d y:%d,y_last_w:%d\n",x,y,sta[y].op_last->w);
             }else{
-                sta[y] = sta[x];
+                sta[y].sum = sta[x].sum;
+                sta[y].op_last = sta[x].op_begin;
+                sta[y].op_begin = sta[x].op_last;
             }
-            sta[x].op_begin->if_rev = !sta[x].op_begin->if_rev;
 
             cout << sta[y].sum << "\n";
         }
